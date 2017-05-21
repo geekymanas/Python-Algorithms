bb2c965e625476a2eea728166bddbdc0f4d6bd5c
geekymanas <gundapunenimanas@gmail.com>
2017-05-21 03:53:21 +0000

    First commit

---
+++ Untitled1.py
@@ -0,0 +1,10 @@
+x = int(0)
+max_no = int(0)
+while x < 10:
+ y = int(raw_input('Enter the Integer '))
+ if y%2 != 0 and (y > max_no or max_no == 0):
+  max_no = y 
+ x = x + 1
+print 'There is no Odd Number' if max_no == 0 else max_no
+  
+ 
