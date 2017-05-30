186b17fa77419306b584f0c46dff02682c508779
geekymanas <gundapunenimanas@gmail.com>
2017-05-30 06:08:58 +0000

    second commit

--- newton-Raphson.py
+++ newton-Raphson.py
@@ -2,6 +2,8 @@
 epsilon = 0.1
 k = float(raw_input('Enter The Number '))
 guess = k/2.0
+num_guess = 0.0
 while abs(guess*guess - k) >= epsilon:
+    num_guess += num_guess
     guess = guess-(((guess**2) - k)/(2*guess))
 print 'Square Root Of ', k , 'is close to ', guess
